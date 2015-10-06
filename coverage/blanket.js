86986163aedcdd3fd541a02a028f3265c1f52c29
Justin Livi <justin@brinkbit.com>
2015-10-05 21:38:31 -0400

    Add and track coverage/blanket.js
    .gitignore was set to ignore blanket coverage instrumentation. Only
    coverage/codeCoverage/html should be untracked.

---
+++ coverage/blanket.js
@@ -0,0 +1,7 @@
+const path = require( 'path' );
+const srcDir = path.join( __dirname, '..', 'src' );
+
+require( 'blanket' )({
+    // Only files that match the pattern will be instrumented
+    pattern: srcDir,
+});
