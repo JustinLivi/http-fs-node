90e1e16e9ab4f48bc01dcfbabbee831c8c19f64c
Justin Livi <justin@brinkbit.com>
2015-10-05 21:53:21 -0400

    Refactor to pass linting

--- test/functional/get.js
+++ test/functional/get.js
@@ -1,19 +1,22 @@
-var chai = require( 'chai' ),
-    expect = chai.expect,
-    chaiaspromised = require( 'chai-as-promised' ), // more complicated; look that shit up
-    sinonchai = require( 'sinon-chai' ),
-    sinon = require( 'sinon' ),
-    get = require( '../../src/get.js' );
+/* eslint-env node, mocha */
+/* eslint no-unused-expressions: 0, func-names: 0 */
 
-    chai.use( sinonchai );
-    chai.use( chaiaspromised );
+const chai = require( 'chai' );
+const expect = chai.expect;
+const chaiaspromised = require( 'chai-as-promised' );
+const sinonchai = require( 'sinon-chai' );
+// const sinon = require( 'sinon' );
+const get = require( '../../src/get.js' );
 
-describe( 'homepage' , function() {
-    it( 'should respond be true' , function() {
+chai.use( sinonchai );
+chai.use( chaiaspromised );
+
+describe( 'homepage', function() {
+    it( 'should respond be true', function() {
         expect( true ).to.be.true;
     });
 
-    it( 'should equal 3, cause' , function() {
-        return expect( get() ).to.eventually.equal( 3 );
+    it( 'should equal 3, cause', function() {
+        return expect( get()).to.eventually.equal( 3 );
     });
 });
